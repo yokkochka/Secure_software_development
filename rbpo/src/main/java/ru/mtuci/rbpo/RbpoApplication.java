e27becf1de4fc31bd073bcd2efb082f3208ca2ff
test <ulyana.yunusova@bk.ru>
2024-09-14 22:19:58 +0300

    init

---
+++ rbpo/src/main/java/ru/mtuci/rbpo/RbpoApplication.java
@@ -0,0 +1,13 @@
+package ru.mtuci.rbpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbpoApplication.class, args);
+	}
+
+}
---
+++ rbpo/src/test/java/ru/mtuci/rbpo/RbpoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.rbpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
